828e97e43efd6ea32624a34bd1bf8d0435ae7f59
azimkubatov <kubat.azim@gmail.com>
2024-04-25 11:47:23 +0600

    clean the project

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_rxdart/posts.dart';
-import 'package:flutter_rxdart/posts_bloc.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,49 +8,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final PostsBloc _postsBloc = PostsBloc();
-
   @override
-  void initState() {
-    super.initState();
-    _postsBloc.fetchPosts();
-  }
-
-  @override
-  void dispose() {
-    _postsBloc.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        body: StreamBuilder<List<Posts>>(
-          stream: _postsBloc.postsStream,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              final post = snapshot.data!;
-
-              return ListView.builder(
-                itemCount: post.length,
-                itemBuilder: (ctx, index) => Column(
-                  children: [
-                    Text(
-                      'Name: ${post[2].title}',
-                      style: const TextStyle(fontSize: 18),
-                    ),
-                    Text(
-                      'body: ${post[2].body}',
-                      style: const TextStyle(fontSize: 18),
-                    ),
-                  ],
-                ),
-              );
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
-          },
-        ),
-      );
+  Widget build(BuildContext context) => Scaffold(body: Container());
 }
